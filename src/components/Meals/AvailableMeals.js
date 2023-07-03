d815ae7576e9c571c8694ced3fefd37ad46074fa
SLM <sabrina.martorelli@gmail.com>
2023-07-03 17:25:31 +0100

    Adds MealItem  component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import classes from './AvailableMeals.module.css'
+import MealItem from './MealItem/MealItem';
+import Card from '../UI/Card';
 
 const DUMMY_MEALS = [
     {
@@ -29,13 +31,21 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
 
-    const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const mealList = DUMMY_MEALS.map(meal => 
+    < MealItem 
+    key={meal.id} 
+    name={meal.name}
+    description = {meal.description}
+    price = {meal.price}
+    />);
     return (
         <>
             <section className={classes.meals}>
+                <Card>
                 <ul>
                 {mealList}
                 </ul>
+                </Card>
             </section>
         </>
 
