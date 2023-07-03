615ea755dd04e0f98f692d4963f0a2a1c604afea
SLM <sabrina.martorelli@gmail.com>
2023-07-03 14:33:33 +0100

    Adds style to button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@
 
 import mealsImage from '../../assets/meals.png'
 import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
     return (
         <>
         <header className={classes.header}>
             <h1>HappyMeals</h1>
-            <button>Cart</button>
+          <HeaderCartButton/>
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="Oriental food pieces"/>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,19 @@
 
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
 const HeaderCartButton = (props) => {
     return ( 
 
-<button>
-    <span></span>
-    <span></span>
-    <span></span>
+<button className={classes.button}>
+    <span className={classes.icon}>
+    <CartIcon />
+
+    </span>
+    <span>Your Cart</span>
+    <span className={classes.badge}>
+        3
+    </span>
 </button>
 
 
