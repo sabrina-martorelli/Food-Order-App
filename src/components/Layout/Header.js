dde3ed1e04e1d2897507eb91c30831d92ef4f0c0
SLM <sabrina.martorelli@gmail.com>
2023-07-03 14:15:29 +0100

    Completes header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,10 +1,21 @@
 
+
+import mealsImage from '../../assets/meals.png'
+import classes from './Header.module.css'
+
 const Header = (props) => {
     return (
+        <>
+        <header className={classes.header}>
+            <h1>HappyMeals</h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="Oriental food pieces"/>
 
+        </div>
+        </>
 
-
-        
       );
 }
  
