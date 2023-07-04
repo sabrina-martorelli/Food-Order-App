f7e0cb742badd71992bee637219d7a14b729c33b
SLM <sabrina.martorelli@gmail.com>
2023-07-04 10:28:30 +0100

    Adds Modal / Portal component

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,50 @@
+
+
+import classes from './Modal.module.css';
+import ReactDOM from 'react-dom';
+
+const Backdrop = props => {
+
+return (
+
+<div className = {classes.backdrop}></div>
+
+);
+
+};
+
+const ModalOverlay = props => {
+
+<div className = {classes.modal}>
+
+<div className = {classes.content}>
+    {props.children}
+</div>
+
+</div>
+
+
+};
+
+const portalElement = document.getElementById('overlays');
+
+
+const Modal = (props) => {
+    return ( 
+        <>
+        // Solution without using the portal
+        // <>
+        // <Backdrop />
+        // <ModalOverlay>{props.children}</ModalOverlay>
+        // </>
+
+        // Solution using Portal
+        {ReactDOM. createPortal(<Backdrop/>, portalElement)}
+
+        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+        </>
+
+     );
+}
+ 
+export default Modal;
